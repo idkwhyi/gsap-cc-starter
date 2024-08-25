111dfcf7857272cfc96307726f992892a7aa5a38
Endelif <92841501+idkwhyi@users.noreply.github.com>
2024-08-25 19:46:57 +0700

    Learn GSAP To

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,6 +1,21 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
 
+  useGSAP(() => {
+    gsap.to('#blue-box', {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360, // degree
+      duration: 2, // second
+      ease: 'elastic',
+    })
+  }, []);
+
+  //* Please read GSAP documentation link below for more information
   return (
     <main>
       <h1>GsapTo</h1>
